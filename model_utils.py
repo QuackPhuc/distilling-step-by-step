d40323705a3ef6c695f4cded067a63db3d179afe
QuackPhuc <tannhathasagi28@gmail.com>
2026-03-23 22:16:09 +0700

    fix: add **kwargs to compute_loss for transformers compat

--- model_utils.py
+++ model_utils.py
@@ -46,7 +46,7 @@ class TaskPrefixTrainer(Seq2SeqTrainer):
         self.output_rationale = output_rationale
 
 
-    def compute_loss(self, model, inputs, return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False, **kwargs):
         pred_outputs = model(**inputs['pred'])
         expl_outputs = model(**inputs['expl'])
 
